293f6a63519340919515ab96eb836c228c655456
Saurav <sp.gharti@gmail.com>
2020-07-17 07:59:34 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomText
+
+admin.site.register(CustomText)
 
 # Register your models here.
--- test_dj_app_2/wsgi.py
+++ test_dj_app_2/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_dj_app_2.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_dj_app_2.settings")
 
 application = get_wsgi_application()
